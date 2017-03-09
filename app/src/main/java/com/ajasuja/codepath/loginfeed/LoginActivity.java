489de57b56ba86765d05e40dc116b90e29e7fe97
Ankit Jasuja <AJasuja@walmartlabs.com>
2017-03-08 19:14:35 -0800

    added background image and toast

--- app/src/main/java/com/ajasuja/codepath/loginfeed/LoginActivity.java
+++ app/src/main/java/com/ajasuja/codepath/loginfeed/LoginActivity.java
@@ -2,6 +2,8 @@ package com.ajasuja.codepath.loginfeed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLogIn(View view) {
+        Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
     }
 }
